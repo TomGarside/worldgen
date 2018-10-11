7bc5954f46db74fd270db76a43753365fca28ff1
Tom Garside <tom31@hotmail.co.uk>
2018-10-10 22:53:39 -0400

    Initial Commit

---
+++ worldgen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  worldgen
+//
+//  Created by tom garside on 10/10/18.
+//  Copyright © 2018 tom garside. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
